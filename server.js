b4451a697cd55f726315c51d8742fd775b159637
Ali Raza <ali.backend@fit-outfit.com>
2021-05-06 04:26:36 +0500

    fixed

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = 3000;
+const PORT = 8081;
 
 app.use(express.static(path.join(__dirname, 'book-management', 'build')))
 
