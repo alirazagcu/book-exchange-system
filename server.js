3564167de910510587ddd3be85b362159cc68551
Ubuntu <ubuntu@ip-172-31-36-154.ap-south-1.compute.internal>
2021-05-06 16:48:26 +0000

    fixed server issue

--- server.js
+++ server.js
@@ -6,6 +6,14 @@ const PORT = 8081;
 
 app.use(express.static(path.join(__dirname, 'book-management', 'build')))
 
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'book-management/build/index.html'), function(err) {
+       if (err) {
+         res.status(500).send(err)
+       }
+    })
+})
+
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`)
 })
